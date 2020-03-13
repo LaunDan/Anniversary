0258eb8c15df865659b9e9767194fe6a7cf5fbe3
LaunDan <launadaniel2@gmail.com>
2020-03-13 21:01:54 +0100

    add jlabels and buttons

--- src/OverviewJFrame.java
+++ src/OverviewJFrame.java
@@ -2,13 +2,22 @@ import javax.swing.*;
 
 public class OverviewJFrame extends JFrame {
     private JPanel Anniversary;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JList list1;
+    private JLabel todayJLabel;
+    private JLabel soonestJLabel;
+    private JLabel birthdayJLabel;
+    private JLabel ageJLabel;
 
-    public void OverviewJFrame(){
+    public OverviewJFrame() {
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(Anniversary);
         this.pack();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         JFrame frame = new OverviewJFrame();
         frame.setTitle("Anniversary");
         frame.setSize(400, 400);
