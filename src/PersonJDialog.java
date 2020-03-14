39e2c3ebb671c44ef3f0f834d0bd167f4377ca67
LaunDan <launadaniel2@gmail.com>
2020-03-14 20:05:20 +0100

    set diaog

--- src/PersonJDialog.java
+++ src/PersonJDialog.java
@@ -1,20 +1,17 @@
 import javax.swing.*;
 
 public class PersonJDialog extends JDialog {
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+    private JPanel AddPerson;
+    private JButton OKButton;
+    private JTextField nameTextField;
+    private JFormattedTextField birthdayFirmattedTextField;
+
 
     public PersonJDialog() {
-        setContentPane(contentPane);
+        setContentPane(AddPerson);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+
     }
 
-    public static void main(String[] args) {
-        PersonJDialog dialog = new PersonJDialog();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
-    }
+
 }
