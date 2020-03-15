4f5902e144967f1b3371e6466e20331879acbb9a
LaunDan <launadaniel2@gmail.com>
2020-03-15 13:30:57 +0100

    create properities of person

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,26 @@
+import java.time.LocalDate;
+
 public class Person {
+    private String name;
+    private LocalDate birthday;
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person(String name, LocalDate birthday) throws IllegalArgumentException{
+        if (name.length() < 3){
+            throw new IllegalArgumentException("Name is too short..");
+        }
+        if (birthday.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("Date of birthday can't be in the future..")
+        }
+
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
