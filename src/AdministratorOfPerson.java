9764b3ed8780a7290d90f81c94267f8b83e988d9
LaunDan <launadaniel2@gmail.com>
2020-03-15 13:54:49 +0100

    set class administrator of persons

--- src/AdministratorOfPerson.java
+++ src/AdministratorOfPerson.java
@@ -1,3 +1,24 @@
+import javax.swing.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class AdministratorOfPerson {
-    
+    private DefaultListModel<Person> persons = new DefaultListModel<>();
+
+    public ListModel getModel(){
+        return persons;
+    }
+
+    public void add(Person person){
+        persons.addElement(person);
+    }
+
+    public void delete(Person person){
+        persons.removeElement(person);
+    }
+
+    public List<Person> getPersons(){
+        return Collections.list(persons.elements());
+    }
 }
